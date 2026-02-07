4e51769e480d0b14891e64de7fb681e0b160b4f2
minermouse <152760463+mouse-m@users.noreply.github.com>
2026-02-07 12:59:56 +0800

    Create STAR_CPP.h

---
+++ STAR_CPP.h
@@ -0,0 +1,27 @@
+/*
+DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+Version 2, December 2004
+
+Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+
+Everyone is permitted to copy and distribute verbatim or modified
+copies of this license document, and changing it is allowed as long
+as the name is changed.
+
+DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+
+0. You just DO WHAT THE FUCK YOU WANT TO.
+
+*/
+
+#ifndef STAR_H
+#define STAR_H
+
+#include "star/star_io.h"
+#include "star/star_graph.h"
+#include "star/star_random.h"
+#include "star/star_sequence.h"
+#include "star/star_math.h"
+#include "star/extend/StarExtendedRandomEngine.h"
+#endif
